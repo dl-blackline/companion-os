837efcd432173bc685ad66ff0f31314251c2a972
Dalton <dl@blacklinecrm.com>
2026-04-02 11:57:04 -0400

    fix: show exact cents in currency formatting (was rounding to whole dollars)

--- src/components/views/FinanceView.tsx
+++ src/components/views/FinanceView.tsx
@@ -38,7 +38,8 @@ function currency(value: number) {
   return new Intl.NumberFormat('en-US', {
     style: 'currency',
     currency: 'USD',
-    maximumFractionDigits: 0,
+    minimumFractionDigits: 2,
+    maximumFractionDigits: 2,
   }).format(value);
 }
 
